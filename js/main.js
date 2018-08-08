1bab1ff3e931d33468f3bc3d8ad7cfabc76bc983
rahulgupta20 <dearrahulgupta4@gamil.com>
2018-08-08 16:03:24 +0545

    Adjusted munus design in responsive case.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-// $(document).ready(function() {
-//     $(".navbar").sticky({ topSpacing: 0, zIndex: 999 });
-// });
-
-
 $(document).ready(function() {
-        $("#header").sticky({ topSpacing: 0, zIndex: '9999' });
-        $('#header').on('sticky-start', function() { $('.navbar-dark').removeClass('bg-dark').addClass('navbar-light bg-white').removeClass('navbar-dark'); });
-        $('#header').on('sticky-end', function() { $('.navbar-light').removeClass('bg-white').addClass('navbar-dark bg-dark').removeClass('navbar-light'); });
-    });
+    $("#header").sticky({ topSpacing: 0, zIndex: '9999' });
+    $('#header').on('sticky-start', function() { $('.navbar-dark').removeClass('bg-dark').addClass('navbar-light bg-white').removeClass('navbar-dark'); });
+    $('#header').on('sticky-end', function() { $('.navbar-light').removeClass('bg-white').addClass('navbar-dark bg-dark').removeClass('navbar-light'); });
+});
+
+$(document).click(function(event) {
+    var clickover = $(event.target);
+    var $navbar = $(".navbar-collapse");
+    var _opened = $navbar.hasClass("show");
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) {
+        $navbar.collapse('hide');
+    }
+});
